459c5a9311ac5ce2754adc0860416292b27c3f85
Rajshri Patil <rajshripatil19022006@gmail.com>
2025-11-09 22:54:53 +0530

    formatted the file

--- routes/course.js
+++ routes/course.js
@@ -2,17 +2,17 @@ const { Router } = require("express")
 const courseRouter = Router();
 
 
-    courseRouter.get("/preview", function(req, res){
-        res.send({
-            msg: "course preview endpoint"
-        })
+courseRouter.get("/preview", function (req, res) {
+    res.send({
+        msg: "course preview endpoint"
     })
+})
 
-    courseRouter.post("/purchase", function(req, res){
-        res.send({
-            msg: "course purchase endpoint"
-        })
+courseRouter.post("/purchase", function (req, res) {
+    res.send({
+        msg: "course purchase endpoint"
     })
+})
 
 
 module.exports = {
